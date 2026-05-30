ce961bc70478cc545738cd57a7ef5eaa55a552d2
iragena25egide <iragenaegide205@gmail.com>
2026-05-30 14:11:45 +0200

    new project module

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProjectService } from './projects.service';
 import { ProjectController } from './projects.controller';
+import { NotificationsModule } from '../auth/notification.module';
 
 @Module({
+  imports: [NotificationsModule],
   providers: [ProjectService],
-  controllers: [ProjectController]
+  controllers: [ProjectController],
+  exports: [ProjectService],
 })
 export class ProjectsModule {}
