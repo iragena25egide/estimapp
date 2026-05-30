a8444bb6464260c09059d572ace74f89eed01767
iragena25egide <iragenaegide205@gmail.com>
2026-05-30 14:02:20 +0200

    boq controller new

--- src/boq-item/boq-item.controller.ts
+++ src/boq-item/boq-item.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   UseGuards,
+  Req,
 } from '@nestjs/common';
 import { BoqItemService } from './boq-item.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
@@ -17,27 +18,27 @@ export class BoqItemController {
   constructor(private readonly service: BoqItemService) {}
 
   @Post('create')
-  create(@Body() body: any) {
-    return this.service.create(body);
+  create(@Body() body: any, @Req() req: any) {
+    return this.service.create(body, req.user.id);
   }
 
   @Get('project/:projectId')
-  findByProject(@Param('projectId') projectId: string) {
-    return this.service.findByProject(projectId);
+  findByProject(@Param('projectId') projectId: string, @Req() req: any) {
+    return this.service.findByProject(projectId, req.user.id);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.service.findOne(id);
+  findOne(@Param('id') id: string, @Req() req: any) {
+    return this.service.findOne(id, req.user.id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body: any) {
-    return this.service.update(id, body);
+  update(@Param('id') id: string, @Body() body: any, @Req() req: any) {
+    return this.service.update(id, body, req.user.id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.service.remove(id);
+  remove(@Param('id') id: string, @Req() req: any) {
+    return this.service.remove(id, req.user.id);
   }
 }
